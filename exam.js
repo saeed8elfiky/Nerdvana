7f2f3c113570054203cc6932644f77fab9edea04
Saeed Elfiky <73163770+saeed8elfiky@users.noreply.github.com>
2025-05-01 07:43:33 +0300

    Update exam.js

--- exam.js
+++ exam.js
@@ -234,7 +234,7 @@ const getNewQuestion = () => {
 
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
-        return window.location.assign('/end.html');
+        return window.location.assign('end.html');
     }
 
     questionCounter++;
