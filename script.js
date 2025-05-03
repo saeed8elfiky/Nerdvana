a5ae12191ece3a042fd46d0a799aae1d08a7d559
Saeed Elfiky <73163770+saeed8elfiky@users.noreply.github.com>
2025-05-04 02:21:20 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('acceptCookies').addEventListener('click', function() {
-    document.getElementById('cookieConsent').style.display = 'none';
-    // Here you can add code to set a cookie indicating consent
-});
-  
